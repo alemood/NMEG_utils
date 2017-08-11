6abd77183b46ba350462c2f94b1685db5e74aecc
Alex Moody <alexmoody@unm.edu>
2017-08-11 10:06:01 -0600

    Update paths to SANDIA and add some provisional PRISM

--- output_file_scripts/getPRISMdata.py
+++ output_file_scripts/getPRISMdata.py
@@ -13,7 +13,7 @@ Maybe this should be rewritten to use fiona for file loading
 # must be appended first
 
 import sys
-sys.path.append( '/home/greg/current/NMEG_utils/py_modules/' )
+sys.path.append( 'C:/Code/NMEG_utils/py_modules/' )
 import bilParser as bf
 
 # import other packages
@@ -23,14 +23,16 @@ import pdb
 # NOTE - Manually change the month in the PRISM_daily directory and
 # date range if doing an incomplete year dataset
 # (current year to 6 months after)
-years = list(range(2007, 2017))
-site_coords_file = '/home/greg/current/NMEG_utils/site_coords.txt'
+years = list(range(2016, 2017))
+site_coords_file = 'C:/Code/NMEG_utils/site_coords.txt'
 make_plot=False
 
 
 # First extract the daily precip data
-out_path = r'/home/greg/sftp/eddyflux/Ancillary_met_data/PRISM_daily/'
+out_path = r'/Research_Flux_Towers/Ancillary_met_data/PRISM_daily/'
+out_path = 'C:/Research_Flux_Towers/Ancillary_met_data/PRISM_daily/'
 bil_path = out_path + 'raw_bil/'
+#%%
 for yr in years:
     a = bf.getDailyPrism(yr, 'ppt', bil_path, site_coords_file)
     if make_plot:
@@ -42,15 +44,43 @@ for yr in years:
     a.to_csv(out_path + 'PRISM_Daily_ppt_{0}.csv'.format(yr), 
             index_label='date')
 pdb.set_trace()
+#%%
 # Code for making provisional files (can be appended to incomplete year data)
-#bil_name = 'PRISM_ppt_provisional_4kmD2_20151201_20151231_bil.zip'
-#prov = bf.getDailyPrismProvis(2015, 12, 'ppt', bil_path, bil_name, 
-#        site_coords_file) 
-#prov.to_csv(out_path + 'PRISM_DailyProvis_ppt_Dec2015.csv')
+bil_name = 'PRISM_ppt_provisional_4kmD2_20161201_20161231_bil.zip'
+prov = bf.getDailyPrismProvis(2016, 12, 'ppt', bil_path, bil_name, 
+        site_coords_file) 
+prov.to_csv(out_path + 'PRISM_DailyProvis_ppt_Dec2016.csv')
 
+bil_name = 'PRISM_ppt_provisional_4kmD2_20170101_20170131_bil.zip'
+prov = bf.getDailyPrismProvis(2017, 1, 'ppt', bil_path, bil_name, 
+        site_coords_file) 
+prov.to_csv(out_path + 'PRISM_DailyProvis_ppt_Jan2017.csv')
+
+bil_name = 'PRISM_ppt_provisional_4kmD2_20170201_20170228_bil.zip'
+prov = bf.getDailyPrismProvis(2017, 2, 'ppt', bil_path, bil_name, 
+        site_coords_file) 
+prov.to_csv(out_path + 'PRISM_DailyProvis_ppt_Feb2017.csv')
+
+bil_name = 'PRISM_ppt_provisional_4kmD2_20170301_20170331_bil.zip'
+prov = bf.getDailyPrismProvis(2017, 3, 'ppt', bil_path, bil_name, 
+        site_coords_file) 
+prov.to_csv(out_path + 'PRISM_DailyProvis_ppt_Mar2017.csv')
+
+bil_name = 'PRISM_ppt_provisional_4kmD2_20170401_20170430_bil.zip'
+prov = bf.getDailyPrismProvis(2017, 4, 'ppt', bil_path, bil_name, 
+        site_coords_file) 
+prov.to_csv(out_path + 'PRISM_DailyProvis_ppt_Apr2017.csv')
+
+bil_name = 'PRISM_ppt_provisional_4kmD2_20170501_20170531_bil.zip'
+prov = bf.getDailyPrismProvis(2017, 5, 'ppt', bil_path, bil_name, 
+        site_coords_file) 
+prov.to_csv(out_path + 'PRISM_DailyProvis_ppt_May2017.csv')
+
+
+#%%
 
 # Then monthly precip and temperature data
-out_path = r'/home/greg/sftp/eddyflux/Ancillary_met_data/PRISM_monthly/'
+out_path = r'/Research_Flux_Towers/Ancillary_met_data/PRISM_monthly/'
 bil_path = out_path + 'raw_bil/'
 met_types = ['ppt', 'tmean']
 for t in met_types:
@@ -60,15 +90,30 @@ for t in met_types:
         a.plot()
         plt.show()
     print(a.sum())
-    a.to_csv(out_path + 'PRISM_Monthly_{0}_1981_2015.csv'.format(t), 
+    a.to_csv(out_path + 'PRISM_Monthly_{0}_1981_2016.csv'.format(t), 
             index_label='date')
 
-
+#%%
 #Code for making provisional files (can be appended to incomplete year data)
-bil_name = 'PRISM_tmean_provisional_4kmM2_201510_201603_bil.zip'
-prov = bf.getMonthlyPrismProvis(2015, 'tmean', bil_path, bil_name, 
+#bil_name = 'PRISM_tmean_provisional_4kmM2_201612_201705_bil.zip'
+#prov = bf.getMonthlyPrismProvis(2016, 'tmean', bil_path, bil_name, 
+#        site_coords_file) 
+#prov.to_csv(out_path + 'PRISM_MonthlyProvis_tmean_2016.csv')
+
+#bil_name = 'PRISM_tmean_provisional_4kmM2_201612_201705_bil.zip'
+#prov = bf.getMonthlyPrismProvis(2017, 'tmean', bil_path, bil_name, 
+#        site_coords_file) 
+#prov.to_csv(out_path + 'PRISM_MonthlyProvis_tmean_2017.csv')
+
+#bil_name = 'PRISM_ppt_provisional_4kmM2_201612_201705_bil.zip'
+#prov = bf.getMonthlyPrismProvis(2016, 'tmean', bil_path, bil_name, 
+#        site_coords_file) 
+#prov.to_csv(out_path + 'PRISM_MonthlyProvis_ppt_2016.csv')
+##
+bil_name = 'PRISM_ppt_provisional_4kmM3_201612_201705_bil.zip'
+prov = bf.getMonthlyPrismProvis(2016, 'ppt', bil_path, bil_name, 
         site_coords_file) 
-prov.to_csv(out_path + 'PRISM_MonthlyProvis_tmean_2015.csv')
+prov.to_csv(out_path + 'PRISM_MonthlyProvis_ppt_2016.csv')
 
 
 # Now change bil_path and extract the 30yr normal precip
